67c102f6a39f1346fe9063df63f0d1d22dd73cfe
Sebastian Idziak <228199@student.pwr.edu.pl>
2019-02-23 00:47:20 +0100

    Implemented sight for barrel

---
+++ Assets/Scripts/SightHandler.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class SightHandler : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject sightToSpawn;
+
+    private RectTransform sight;
+    private Canvas canvas;
+
+    private void Start()
+    {
+        sight = Instantiate(sightToSpawn).GetComponent<RectTransform>();
+        canvas = FindObjectOfType<Canvas>();
+        sight.SetParent(canvas.transform);
+    }
+
+    private void Update()
+    {
+        sight.position = Camera.main.WorldToScreenPoint(transform.position);
+        Quaternion rot = sight.rotation;
+        rot.eulerAngles = rot.eulerAngles.With(z: -transform.rotation.eulerAngles.y);
+        sight.rotation = rot;
+    }
+}
